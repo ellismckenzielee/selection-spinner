d785348134d9db75928d74434dd88e063078f9e2
ellis lee <ellismckenzielee@gmail.com>
2023-04-15 21:31:21 +0100

    remove segment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 console.log('JS is connected to the page!')
 
 const spinner = document.getElementById('spinner');
-console.log(spinner)
+
+const number_of_segments = 4;
+const colors = ['yellow', 'black', 'green', 'teal']
+const create_segments = (spinner, number_of_segments) => {
+    const increment = 360 / number_of_segments;
+    
+}
+
+create_segments(spinner,  number_of_segments)
