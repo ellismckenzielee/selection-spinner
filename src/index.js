665ae080cbfe27122891dd9e8df0ebed8eb497d0
ellis lee <ellismckenzielee@gmail.com>
2023-04-16 13:34:31 +0100

    add multiple segments and spinning animation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 console.log('JS is connected to the page!')
 
 const spinner = document.getElementById('spinner');
-
-const number_of_segments = 4;
+const spinnerCenter = document.getElementById('spinner-center');
+const numberOfSegments = 4;
+let rotation = 200;
 const colors = ['yellow', 'black', 'green', 'teal']
-const create_segments = (spinner, number_of_segments) => {
-    const increment = 360 / number_of_segments;
+const create_segments = (spinner, numberOfSegments) => {
+    const increment = 360 / numberOfSegments;
     
 }
 
-create_segments(spinner,  number_of_segments)
+
+const setup = () => {
+    spinnerCenter.addEventListener('click', () => {
+        console.log(true)
+        spinner.style.transition = 'transform 5s ease-out';
+        spinner.style.transitionTimingFunction = 'cubic-bezier(0,.67,.46,1.03)';
+        const newRotation = rotation + 100;
+        rotation = newRotation;
+        console.log(newRotation)
+        spinner.style.transform = `rotate(${newRotation}deg)`;
+    })
+}
+
+setup()
