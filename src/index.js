8d698527978c86599615575fc3142cf369f3eb3c
ellis lee <ellismckenzielee@gmail.com>
2023-04-29 16:58:28 +0100

    add letters to segments

--- src/index.js
+++ src/index.js
@@ -23,16 +23,27 @@ const colours = [
   'purple',
   'blue',
   'white',
+  'seagreen',
+  'lightgray',
 ];
 
-const numOfSegments = 4;
+const numOfSegments = 7;
 
 const addSegments = (angle, coord) => {
   for (let segNum = 0; segNum < numOfSegments; segNum += 1) {
     const segment = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+    const text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
     path.setAttribute('d', `M 50,50 L 50,5 A45,45 1 0,1 ${coord.x},${coord.y} z`);
+    text.setAttribute('x', '10');
+    text.setAttribute('y', '70');
+    text.style.transform = `rotate(-${angle}deg)`;
+    text.textContent = segNum;
+    text.setAttribute('id', 'segment-text');
+
+    // text.setAttribute('font-size', 'smaller')
     segment.appendChild(path);
+    segment.appendChild(text);
     segment.setAttribute('id', 'segment');
     segment.setAttribute('viewBox', '0 0 100 100');
     segment.style.transform = `rotate(${angle * segNum}deg)`;
