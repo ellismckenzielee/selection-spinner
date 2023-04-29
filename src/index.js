7cd4f7ab7eea1070c968c47ef679e70b632c6a83
ellis lee <ellismckenzielee@gmail.com>
2023-04-29 12:15:18 +0100

    create any number of segments

--- src/index.js
+++ src/index.js
@@ -17,20 +17,32 @@ const setup = () => {
 
 setup();
 
-const addSegments = (angle, coord) => {
-  const numOfSegments = 4;
+const colours = [
+  'green',
+  'red',
+  'pink',
+  'yellow',
+  'purple',
+  'blue',
+  'white',
+];
 
-  for (let segNum = 1; segNum <= numOfSegments; segNum += 1) {
+const numOfSegments = 3;
+
+const addSegments = (angle, coord) => {
+  for (let segNum = 0; segNum <= numOfSegments; segNum += 1) {
     const segment = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     path.setAttribute('d', `M 50,50 L 50,5 A45,45 1 0,1 ${coord.x},${coord.y} z`);
     segment.appendChild(path);
-    segment.setAttribute('id', `segment-${segNum}`);
+    segment.setAttribute('id', 'segment');
     segment.setAttribute('viewBox', '0 0 100 100');
+    segment.style.transform = `rotate(${angle * segNum}deg)`;
+    segment.style.fill = colours[segNum];
     spinner.appendChild(segment);
   }
 };
 
-const angle = calculateAngle(4);
+const angle = calculateAngle(numOfSegments);
 const endCoord = calculateEndCoords(angle);
 addSegments(angle, endCoord);
