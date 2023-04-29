c2d580794e14943996da1aaf5d9d417a9c1d5d11
ellis lee <ellismckenzielee@gmail.com>
2023-04-29 15:30:44 +0100

    cleanup rotate transition - add to css and remove from index.js

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ let rotation = 80;
 
 const setup = () => {
   spinnerCenter.addEventListener('click', () => {
-    spinner.style.transition = 'transform 5s ease-out';
-    spinner.style.transitionTimingFunction = 'cubic-bezier(0,.67,.46,1.03)';
     const newRotation = rotation + 100;
     rotation = newRotation;
-    spinner.style.transform = `rotate(${newRotation}deg)`;
+    spinner.style.transform = `rotate(${2000}deg)`;
   });
 };
 
@@ -27,10 +25,10 @@ const colours = [
   'white',
 ];
 
-const numOfSegments = 3;
+const numOfSegments = 4;
 
 const addSegments = (angle, coord) => {
-  for (let segNum = 0; segNum <= numOfSegments; segNum += 1) {
+  for (let segNum = 0; segNum < numOfSegments; segNum += 1) {
     const segment = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
     const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
     path.setAttribute('d', `M 50,50 L 50,5 A45,45 1 0,1 ${coord.x},${coord.y} z`);
